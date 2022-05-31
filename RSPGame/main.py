b2a87f6a64349506280b0986a9af5811ae03b9ee
Husseinat Etti-Balogun <hebalogun@gmail.com>
2022-05-31 11:46:38 +0100

    added functions to rsp game2

--- RSPGame/main.py
+++ RSPGame/main.py
@@ -1,12 +1,22 @@
+from random import random
+
+
 play_options = {
     R: "rock", 
     P: "paper", 
     S: "scissors"}
 
-def user_play_rps(user_input):
-    if user_input = input("Type youy play [R, P, S]:")
+def user_play_rps(play):
+    play = input("Type your play [R, P, S]:")
+    for option in play_options:
+        if play == option:
+            print("user played", option)
 
 def cpu_play_rps():
+    play = random.choice(R, P, S)
+    for option in play_options:
+        if play == option:
+            print("cpu played", option)
     
 
 
