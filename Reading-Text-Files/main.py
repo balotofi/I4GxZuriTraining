bc3831594185a6686daf16daa71727391d06ff0d
Husseinat Etti-Balogun <hebalogun@gmail.com>
2022-06-05 20:40:00 +0100

    Update main.py

--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -4,13 +4,16 @@
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
 def read_file_content(filename):
-    # [assignment] Add your code here 
-    
-    return "Hello World"
+    # [assignment] Add your code here
+    with open(f"{filename}") as f:
+    lines = f.readlines
+    return lines
+    #return "Hello World"
 
 
 def count_words():
     text = read_file_content("./story.txt")
     # [assignment] Add your code here
+    for word in lines:
 
     return {"as": 10, "would": 20}
