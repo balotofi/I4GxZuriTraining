5a9b93fe8dc2a715bbb5d7df47195110683772f8
Husseinat Etti-Balogun <hebalogun@gmail.com>
2022-06-06 21:51:02 +0100

    code

--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -5,23 +5,26 @@
 
 def read_file_content(filename):
     # [assignment] Add your code here
-    read = open(f"{filename}.txt", "r")
-    return read
+    with open(f"{filename}.txt") as file_text:
+        contents = file_text.read()
+        return contents
 
 
 def count_words():
-    text = read_file_content("./story.txt")
+    text = read_file_content("story")
     # [assignment] Add your code here
     word_bank = dict()
     for line in text:
-	    line = line.strip()
-	    line = line.lower()
-	    words = line.split(" ")
-	    for word in words:
-		    if word in word_bank:
-			    word_bank[word] = word_bank[word] + 1
-		    else:
-			    word_bank[word] = 1
-
+        line = line.strip()
+        line = line.lower()
+        words = line.split(" ")
+        for word in words:
+            if word in word_bank:
+                word_bank[word] = word_bank[word] + 1
+            else:
+                word_bank[word] = 1
     for key in list(word_bank.keys()):
-	    print(key, ":", word_bank[key])
+        print(key, ":", word_bank[key])
+
+
+count_words()
