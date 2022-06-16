ecbcc86f796027c42630658563de0d3632418c03
Husseinat Etti-Balogun <hebalogun@gmail.com>
2022-06-16 23:22:50 +0100

    created and ran migrations to intialise post model schema

--- I4G011009UMV/blog/models.py
+++ I4G011009UMV/blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField
-    author = models.ForeignKey('Curent user model', models.get_user_model)
+    User = get_user_model()
+    author = models.ForeignKey( User, on_delete=models.CASCADE,)
     created_date = models.DateTimeField('date created')
     published_date = models.DateTimeField('date published')
