8e9dd0cd944d6268b6417a3f6dd57c2d872ee24c
Husseinat Etti-Balogun <hebalogun@gmail.com>
2022-06-16 23:09:18 +0100

    created model in blog app called post

--- I4G011009UMV/blog/models.py
+++ I4G011009UMV/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField
+    author = models.ForeignKey('Curent user model', models.get_user_model)
+    created_date = models.DateTimeField('date created')
+    published_date = models.DateTimeField('date published')
