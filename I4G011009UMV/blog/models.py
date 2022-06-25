bcd46ba71991d8a858dc365e83863728f7981221
Husseinat Etti-Balogun <hebalogun@gmail.com>
2022-06-25 20:45:52 +0100

    Update models.py
    CRUD TASK - Replace models file content

--- I4G011009UMV/blog/models.py
+++ I4G011009UMV/blog/models.py
@@ -1,11 +1,44 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.utils import timezone
+from django.urls import reverse
+from django.template.defaultfilters import slugify
 # Create your models here.
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField
-    User = get_user_model()
-    author = models.ForeignKey( User, on_delete=models.CASCADE,)
-    created_date = models.DateTimeField('date created')
-    published_date = models.DateTimeField('date published')
+
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published")
+    )
+
+    # DB Fields
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=300, unique=True, editable=False)
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    body = models.TextField()
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="draft"
+    )
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        pass 
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"slug": self.slug})
