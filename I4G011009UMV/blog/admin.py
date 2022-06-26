d33cb26827e22599d858f9ea24af0251f5e3222e
Husseinat Etti-Balogun <hebalogun@gmail.com>
2022-06-26 12:44:14 +0100

    view classes

--- I4G011009UMV/blog/admin.py
+++ I4G011009UMV/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Meta
+from .models import Post
 
 # Register your models here.
 admin.site.register(Post)
--- I4G011009UMV/blog/views.py
+++ I4G011009UMV/blog/views.py
@@ -1,3 +1,27 @@
+from msilib.schema import ListView, CreateView, UpdateView, DetailView
+from typing import List
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post 
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post 
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post 
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
